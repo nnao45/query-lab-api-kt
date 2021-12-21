284de2a0cd632d0cb183390a2134368eceb4fc22
nnao45 <n4sekai5y@gmail.com>
2021-12-21 23:47:53 +0900

    5th

--- src/main/kotlin/com/nnao45/infra/mysql/MySQLContext.kt
+++ src/main/kotlin/com/nnao45/infra/mysql/MySQLContext.kt
@@ -1,6 +1,7 @@
 package com.nnao45.infra.mysql
 
 import org.jetbrains.exposed.sql.Database
+import org.jetbrains.exposed.sql.DatabaseConfig
 
 fun initMysqlClient() {
     val mysqlUser = System.getenv("MYSQL_USER") ?: "admin"
@@ -8,7 +9,10 @@ fun initMysqlClient() {
     val mysqlHost = System.getenv("MYSQL_HOST") ?: "127.0.0.1"
     val mysqlDB = System.getenv("MYSQL_DB") ?: "jq_api"
     val mysqlPort = System.getenv("MYSQL_PORT") ?: "3306"
-    val mysqlOpts = System.getenv("MYSQL_OPTS") ?: "useSSL=false&loc=Asia%2FTokyo"
-    Database.connect("jdbc:mysql://$mysqlHost:$mysqlPort/$mysqlDB?$mysqlOpts", "com.mysql.cj.jdbc.Driver", mysqlUser, mysqlPassword)
+    val mysqlOpts = System.getenv("MYSQL_OPTS") ?: "useSSL=false&loc=Asia%2FTokyo&connectTimeout=10000&socketTimeout=10000"
+    Database.connect(url = "jdbc:mysql://$mysqlHost:$mysqlPort/$mysqlDB?$mysqlOpts",
+                     driver = "com.mysql.cj.jdbc.Driver",
+                     user = mysqlUser,
+                     password = mysqlPassword)
 }
 
