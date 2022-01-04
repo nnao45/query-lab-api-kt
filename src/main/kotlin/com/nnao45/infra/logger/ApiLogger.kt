36046094d1409e3e0a2bf46ace17ee30fbe43e2c
nnao45 <n4sekai5y@gmail.com>
2022-01-04 18:48:40 +0900

    ok

--- src/main/kotlin/com/nnao45/infra/logger/ApiLogger.kt
+++ src/main/kotlin/com/nnao45/infra/logger/ApiLogger.kt
@@ -1,16 +1,6 @@
 package com.nnao45.infra.logger
 
 import mu.KotlinLogging
-import org.jetbrains.exposed.sql.SqlLogger
-import org.jetbrains.exposed.sql.StdOutSqlLogger
-import org.jetbrains.exposed.sql.Transaction
-import org.jetbrains.exposed.sql.statements.StatementContext
 
 val logger by lazy { KotlinLogging.logger {}  }
 
-object DoNotLogger: SqlLogger {
-    override fun log(__context: StatementContext, __transaction: Transaction) {
-        // do nothing
-        return
-    }
-}
