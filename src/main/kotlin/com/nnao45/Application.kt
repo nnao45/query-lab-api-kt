55ee51f2d27391042541c6dc766a5a734755aae6
nnao45 <n4sekai5y@gmail.com>
2022-01-04 09:40:47 +0900

    FibonacchiRepoImpl

--- src/main/kotlin/com/nnao45/Application.kt
+++ src/main/kotlin/com/nnao45/Application.kt
@@ -1,6 +1,7 @@
 package com.nnao45
 
 import com.nnao45.infra.repo.BenchmarkRepoImpl
+import com.nnao45.infra.repo.FibonacchiRepoImpl
 import com.nnao45.plugins.configureDDL
 import com.nnao45.plugins.configureRouting
 import io.ktor.application.*
@@ -11,7 +12,7 @@ import io.ktor.server.netty.*
 fun main() {
     embeddedServer(Netty, port = 8081, host = "0.0.0.0") {
         install(CallLogging)
-        configureRouting(BenchmarkRepoImpl)
-        configureDDL()
+        configureRouting(FibonacchiRepoImpl)
+        // configureDDL()
     }.start(wait = true)
 }
--- src/main/kotlin/com/nnao45/plugins/Routing.kt
+++ src/main/kotlin/com/nnao45/plugins/Routing.kt
@@ -2,11 +2,12 @@ package com.nnao45.plugins
 
 import com.nnao45.infra.logger.logger
 import com.nnao45.infra.repo.BenchmarkRepoImpl
+import com.nnao45.infra.repo.FibonacchiRepoImpl
 import io.ktor.application.*
 import io.ktor.response.*
 import io.ktor.routing.*
 
-fun Application.configureRouting(repo: BenchmarkRepoImpl) {
+fun Application.configureRouting(repo: FibonacchiRepoImpl) {
     logger.info("start routing!")
 
     // Starting point for a Ktor app:
@@ -16,7 +17,8 @@ fun Application.configureRouting(repo: BenchmarkRepoImpl) {
         }
 
         get("/exec") {
-            call.respondText("${repo.exec()}")
+            val param: Long =  call.request.queryParameters["n"]?.toLong() ?: 10
+            call.respondText("${repo.exec(param)}")
         }
     }
 }
